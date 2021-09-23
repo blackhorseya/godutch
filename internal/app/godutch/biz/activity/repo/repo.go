c463a1a9321a1981cdda6d4b0e1a22d93673faf7
Sean Zheng <blackhorseya@gmail.com>
2021-09-23 22:41:16 +0800

    feat: added activity repo interface

---
+++ internal/app/godutch/biz/activity/repo/repo.go
@@ -0,0 +1,31 @@
+package repo
+
+import (
+	"github.com/blackhorseya/godutch/internal/pkg/base/contextx"
+	"github.com/blackhorseya/godutch/internal/pkg/entity/event"
+	"github.com/google/wire"
+)
+
+// IRepo declare activity repository functions
+type IRepo interface {
+	// GetByID serve caller to get an activity by id
+	GetByID(ctx contextx.Contextx, id, userID int64) (info *event.Activity, err error)
+
+	// Create an activity with name and members
+	Create(ctx contextx.Contextx, created *event.Activity) (info *event.Activity, err error)
+
+	// List serve caller to list all activities
+	List(ctx contextx.Contextx, id, userID int64, limit, offset int) (infos []*event.Activity, err error)
+
+	// Count serve caller to count all activities
+	Count(ctx contextx.Contextx, userID int64) (total int, err error)
+
+	// Update serve caller to update an activity information
+	Update(ctx contextx.Contextx, updated *event.Activity) (info *event.Activity, err error)
+
+	// Delete serve caller to delete an activity by id
+	Delete(ctx contextx.Contextx, id, userID int64) error
+}
+
+// ProviderSet is a provider set for wire
+var ProviderSet = wire.NewSet()
--- internal/pkg/entity/event/activity.go
+++ internal/pkg/entity/event/activity.go
@@ -6,6 +6,8 @@ import "github.com/blackhorseya/godutch/internal/pkg/entity/user"
 type Activity struct {
 	ID        int64           `json:"id" db:"id"`
 	Name      string          `json:"name" db:"name"`
+	OwnerID   int64           `json:"-" db:"owner_id"`
+	Owner     *user.Profile   `json:"owner" db:"owner"`
 	Members   []*user.Profile `json:"members" db:"members"`
 	CreatedAt int64           `json:"created_at" db:"created_at"`
 }
