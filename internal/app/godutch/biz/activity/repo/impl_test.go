b599546333ecefef24349e2e663fb1eec5aa22b5
Sean Zheng <blackhorseya@gmail.com>
2021-09-25 00:53:39 +0800

    revert: update create act repo
    This reverts commit 11ed2ee07315cc8a3207dd36e65f101c3d456063.

--- internal/app/godutch/biz/activity/repo/impl_test.go
+++ internal/app/godutch/biz/activity/repo/impl_test.go
@@ -129,7 +129,6 @@ func (s *repoSuite) Test_impl_GetByID() {
 
 func (s *repoSuite) Test_impl_Create() {
 	stmt := "INSERT INTO activities"
-	stmt1 := "INSERT INTO activities_users_map"
 
 	type args struct {
 		created *event.Activity
@@ -152,26 +151,10 @@ func (s *repoSuite) Test_impl_Create() {
 			wantErr:  true,
 		},
 		{
-			name: "insert map then error",
-			args: args{created: act1, mock: func() {
-				s.mock.ExpectExec(stmt).
-					WithArgs(act1.ID, act1.Name, act1.OwnerID, act1.CreatedAt).
-					WillReturnResult(sqlmock.NewResult(1, 1))
-				s.mock.ExpectExec(stmt1).
-					WithArgs(act1.ID, act1.Owner.ID).
-					WillReturnError(errors.New("error"))
-			}},
-			wantInfo: nil,
-			wantErr:  true,
-		},
-		{
 			name: "create then success",
 			args: args{created: act1, mock: func() {
 				s.mock.ExpectExec(stmt).
 					WithArgs(act1.ID, act1.Name, act1.OwnerID, act1.CreatedAt).
-					WillReturnResult(sqlmock.NewResult(1, 1))
-				s.mock.ExpectExec(stmt1).
-					WithArgs(act1.ID, act1.Owner.ID).
 					WillReturnResult(sqlmock.NewResult(1, 1))
 			}},
 			wantInfo: act1,
