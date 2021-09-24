94ff3d5810ef868cfc9a64a217cc5b1471183783
Sean Zheng <blackhorseya@gmail.com>
2021-09-24 12:17:18 +0800

    feat: update 500 errors

--- internal/pkg/entity/er/500.go
+++ internal/pkg/entity/er/500.go
@@ -40,63 +40,23 @@ var (
 )
 
 var (
-	// ErrCreateGoal means create a objective is failure
-	ErrCreateGoal = newAPPError(http.StatusInternalServerError, 50020, "create an objective is failure")
+	// ErrCreateActivity means create an activity is failure
+	ErrCreateActivity = newAPPError(http.StatusInternalServerError, 50020, "create an activity is failure")
 
-	// ErrListGoals means list all objectives is failure
-	ErrListGoals = newAPPError(http.StatusInternalServerError, 50021, "list all objectives is failure")
+	// ErrListActivities means list all activities is failure
+	ErrListActivities = newAPPError(http.StatusInternalServerError, 50021, "list all activities is failure")
 
-	// ErrCountGoal means count all objectives is failure
-	ErrCountGoal = newAPPError(http.StatusInternalServerError, 50023, "count objective is failure")
+	// ErrCountActivity means count all activities is failure
+	ErrCountActivity = newAPPError(http.StatusInternalServerError, 50023, "count activities is failure")
 
-	// ErrUpdateGoal means update a objective is failure
-	ErrUpdateGoal = newAPPError(http.StatusInternalServerError, 50024, "update a objective is failure")
+	// ErrUpdateActivity means update an activity is failure
+	ErrUpdateActivity = newAPPError(http.StatusInternalServerError, 50024, "update an activity is failure")
 
 	// ErrGetActivityByID means get activity by id is failure
-	ErrGetActivityByID = newAPPError(http.StatusInternalServerError, 50025, "get activity by id is failure")
+	ErrGetActivityByID = newAPPError(http.StatusInternalServerError, 50025, "get an activity by id is failure")
 
-	// ErrDeleteGoal means delete a objective by id is failure
-	ErrDeleteGoal = newAPPError(http.StatusInternalServerError, 50026, "delete a objective by id is failure")
-)
-
-var (
-	// ErrCreateTask means create a task is failure
-	ErrCreateTask = newAPPError(http.StatusInternalServerError, 50030, "create a task is failure")
-
-	// ErrUpdateTask means update a task is failure
-	ErrUpdateTask = newAPPError(http.StatusInternalServerError, 50031, "update a task is failure")
-
-	// ErrGetTaskByID means get task by id is failure
-	ErrGetTaskByID = newAPPError(http.StatusInternalServerError, 50032, "get task by id is failure")
-
-	// ErrListTasks means list all tasks is failure
-	ErrListTasks = newAPPError(http.StatusInternalServerError, 50033, "list all tasks is failure")
-
-	// ErrDeleteTask means delete a task is failure
-	ErrDeleteTask = newAPPError(http.StatusInternalServerError, 50034, "delete a task is failure")
-
-	// ErrTaskNotExists means task is not exists
-	ErrTaskNotExists = newAPPError(http.StatusNotFound, 50035, "task is not exists")
-)
-
-var (
-	// ErrGetResultByID means get a key result by id is failure
-	ErrGetResultByID = newAPPError(http.StatusInternalServerError, 50040, "get a key result by id is failure")
-
-	// ErrKRNotExists means key result not exists
-	ErrKRNotExists = newAPPError(http.StatusNotFound, 50041, "key result not exists")
-
-	// ErrListResults means list all key results is failure
-	ErrListResults = newAPPError(http.StatusInternalServerError, 50042, "list all key results is failure")
-
-	// ErrDeleteResult means delete a key result is failure
-	ErrDeleteResult = newAPPError(http.StatusInternalServerError, 50043, "delete a key result is failure")
-
-	// ErrUpdateResult means update a key result is failure
-	ErrUpdateResult = newAPPError(http.StatusInternalServerError, 50044, "update a key result is failure")
-
-	// ErrCreateResult means create a key result is failure
-	ErrCreateResult = newAPPError(http.StatusInternalServerError, 50045, "create a key result is failure")
+	// ErrDeleteActivity means delete an activity by id is failure
+	ErrDeleteActivity = newAPPError(http.StatusInternalServerError, 50026, "delete an activity by id is failure")
 )
 
 var (
