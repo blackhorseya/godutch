48657076144db8e643afcbcf0d56fac9f3e98dc0
Sean Zheng <blackhorseya@gmail.com>
2021-09-25 02:56:45 +0800

    feat: added omitempty into entity fields

--- internal/pkg/entity/user/profile.go
+++ internal/pkg/entity/user/profile.go
@@ -2,10 +2,10 @@ package user
 
 // Profile declare user information
 type Profile struct {
-	ID        int64  `json:"id" db:"id"`
-	Email     string `json:"email" db:"email"`
+	ID        int64  `json:"id,omitempty" db:"id"`
+	Email     string `json:"email,omitempty" db:"email"`
 	Password  string `json:"-" db:"password"`
-	Token     string `json:"token" db:"token"`
-	Name      string `json:"name" db:"name"`
-	CreatedAt int64  `json:"created_at" db:"created_at"`
+	Token     string `json:"token,omitempty" db:"token"`
+	Name      string `json:"name,omitempty" db:"name"`
+	CreatedAt int64  `json:"created_at,omitempty" db:"created_at"`
 }
