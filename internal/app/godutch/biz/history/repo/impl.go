5ff03b870054b7ca4f478e636b9f25846dbfdf4f
Sean Zheng <blackhorseya@gmail.com>
2021-09-26 19:55:36 +0800

    feat: update get by id spend record repo

--- internal/app/godutch/biz/history/repo/impl.go
+++ internal/app/godutch/biz/history/repo/impl.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/blackhorseya/godutch/internal/pkg/base/contextx"
 	"github.com/blackhorseya/godutch/internal/pkg/entity/event"
+	"github.com/blackhorseya/godutch/internal/pkg/entity/user"
 	"github.com/jmoiron/sqlx"
 	"go.uber.org/zap"
 )
@@ -30,20 +31,34 @@ func (i *impl) GetByID(ctx contextx.Contextx, id int64) (info *event.Record, err
 	stmt := `
 select h.id         as id,
        h.remark     as remark,
+       h.total      as total,
        user.id      as "payer.id",
        user.email   as "payer.email",
        user.name    as "payer.name",
-       detail.value as total,
        h.created_at as created_at
 from spend_history h
          join users user on user.id = h.payer_id
-         join spend_details detail on h.payer_id = detail.user_id
-where h.id = ? 
-order by detail.value desc limit 1`
+where h.id = ?`
 	err = i.rw.GetContext(timeout, &ret, stmt, id)
 	if err != nil {
 		return nil, err
 	}
+
+	var members []*user.Member
+	stmt1 := `
+select user.id      as id,
+       user.email   as email,
+       user.name    as name,
+       detail.value as value
+from spend_details detail
+         join users user on detail.user_id = user.id
+where detail.spend_id = ?`
+	err = i.rw.SelectContext(timeout, &members, stmt1, id)
+	if err != nil {
+		return nil, err
+	}
+
+	ret.Members = members
 
 	return &ret, nil
 }
--- internal/app/godutch/biz/history/repo/impl_test.go
+++ internal/app/godutch/biz/history/repo/impl_test.go
@@ -23,7 +23,11 @@ var (
 
 	user1 = &user.Member{Id: userID1}
 
-	record1 = &event.Record{ID: id1, Payer: user1}
+	record1 = &event.Record{
+		ID:      id1,
+		Payer:   user1,
+		Members: []*user.Member{user1},
+	}
 )
 
 type repoSuite struct {
@@ -53,14 +57,20 @@ func (s *repoSuite) Test_impl_GetByID() {
 	stmt := `
 select h.id         as id,
        h.remark     as remark,
+       h.total      as total,
        user.id      as "payer.id",
        user.email   as "payer.email",
        user.name    as "payer.name",
-       detail.value as total,
        h.created_at as created_at
 from spend_history h
-         join users user on user.id = h.payer_id
-         join spend_details detail on h.payer_id = detail.user_id`
+         join users user on user.id = h.payer_id`
+	stmt1 := `
+select user.id      as id,
+       user.email   as email,
+       user.name    as name,
+       detail.value as value
+from spend_details detail
+         join users user on detail.user_id = user.id`
 
 	type args struct {
 		id   int64
@@ -81,11 +91,25 @@ from spend_history h
 			wantErr:  true,
 		},
 		{
-			name: "get by id then success",
+			name: "get members then error",
 			args: args{id: id1, mock: func() {
 				s.mock.ExpectQuery(stmt).WithArgs(id1).
 					WillReturnRows(sqlmock.NewRows([]string{"id", "remark", "payer.id", "payer.email", "payer.name", "total", "created_at"}).
 						AddRow(record1.ID, record1.Remark, record1.Payer.Id, record1.Payer.Email, record1.Payer.Name, record1.Total, record1.CreatedAt))
+				s.mock.ExpectQuery(stmt1).WithArgs(id1).WillReturnError(errors.New("error"))
+			}},
+			wantInfo: nil,
+			wantErr:  true,
+		},
+		{
+			name: "get by id with members then success",
+			args: args{id: id1, mock: func() {
+				s.mock.ExpectQuery(stmt).WithArgs(id1).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "remark", "payer.id", "payer.email", "payer.name", "total", "created_at"}).
+						AddRow(record1.ID, record1.Remark, record1.Payer.Id, record1.Payer.Email, record1.Payer.Name, record1.Total, record1.CreatedAt))
+				s.mock.ExpectQuery(stmt1).WithArgs(id1).
+					WillReturnRows(sqlmock.NewRows([]string{"id", "email", "name", "value"}).
+						AddRow(user1.Id, user1.Email, user1.Name, user1.Value))
 			}},
 			wantInfo: record1,
 			wantErr:  false,
